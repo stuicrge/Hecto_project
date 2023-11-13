266b8384d9cf76ccc5dc93408df2f079c7b572e1
jenny1004 <wosl1006@gmail.com>
2023-11-13 16:43:58 +0900

    test1
    test1

---
+++ API_Agent_Service/src/main/java/com/example/api_agent_service/ApiAgentServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.api_agent_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiAgentServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiAgentServiceApplication.class, args);
+    }
+
+}
---
+++ API_Agent_Service/src/test/java/com/example/api_agent_service/ApiAgentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.api_agent_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiAgentServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

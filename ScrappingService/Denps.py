c64a2b3e8b544f982d530963092872e4bb66a939
jenny1004 <wosl1006@gmail.com>
2023-11-16 13:56:16 +0900

    commit-data1116
    a

--- ScrappingService/Denps.py
+++ ScrappingService/Denps.py
@@ -8,13 +8,13 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import pandas as pd 
 
-
+titlelist = []
 productlist = []
 contentlist = []
 
 # 웹 드라이버 초기화 
 driver = webdriver.Chrome()
-driver.get("https://ckdhcmall.co.kr/prdView.do?prdCode=G2303161200_0051")
+driver.get("https://www.denpsmall.com/goods/goods_view.php?goodsNo=1000000423&crema-product-reviews-1-sort=20")
 driver.maximize_window()
 
 
@@ -23,19 +23,10 @@ time.sleep(2)
 action.send_keys(Keys.END)
 time.sleep(2)
 
-#리뷰 버튼 클릭
-wait = WebDriverWait(driver, 10)  # 10 seconds maximum wait time
-review_button = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="reviewTab"]/a')))
-review_button.click()
-time.sleep(4)
-
-#페이지 넘기는 버튼 스크랩
-page_div = driver.find_element(By.CLASS_NAME, 'paging.reviewPage')
+page_div = driver.find_element(By.CLASS_NAME, 'pagination')
 page_tag = page_div.find_elements(By.TAG_NAME, 'a')
 
-# 페이지 버튼 길이
 page_num=len(page_tag)
-
 
 if page_num >= 5:
     page_num = 6
@@ -44,13 +35,13 @@ if page_num >= 5:
 
         # 페이지 내용 스크래핑
         bs = BeautifulSoup(driver.page_source, 'html.parser')
-        product_review = bs.find(class_="revw_list_area")
-        product_reviews = product_review.find_all("div","rv_info")
+        product_review = bs.find(class_="products_reviews_list")
+        product_reviews = product_review.find_all("div", "review_list_v2__message_container")
 
         print(i)
 
         #페이지 버튼 클릭
-        page_button = driver.find_element(By.XPATH,f'//*[@id="prdDetail"]/div[1]/div/div/div[2]/div/div[3]/div[3]/a[{i+1}]')        
+        page_button = driver.find_element(By.XPATH,f'//*[@id="content"]/div/div/div[7]/div/div/a[i]')        
         time.sleep(4)
         page_button.click()
         time.sleep(4)
@@ -58,8 +49,8 @@ if page_num >= 5:
         #페이지 내용 스크래핑
         for j in range(0, len(product_reviews)):
 
-            content_element = product_reviews[j].select_one('.rv_txt > p')
-            product_element = driver.find_element(By.XPATH, f'//*[@id="contents"]/div/div/div[1]/div[2]/div/div[1]/div[2]/div[1]/h1')
+            content_element = product_reviews[j].select_one('.review_list_v2__expand_link.js-renewal-review-message-link.js-renewal-link-expand.disabled > div')
+            product_element = driver.find_element(By.XPATH, f'//*[@id="frmView"]/div/div[1]/div[1]/h3')
 
             try:
                 content = content_element.text
@@ -73,11 +64,3 @@ if page_num >= 5:
             productlist.append(product_name)
                             
 print("finish")
-
-
-data = {"name":productlist,"content":contentlist}
-df = pd.DataFrame(data)
-print(df.head(100))
-
-df.to_csv("lactofit.csv", encoding = "utf-8-sig")
-driver.quit()

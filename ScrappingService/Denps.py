4ebea4792d16e47decea251995f0f4bdc6c7a736
jenny1004 <wosl1006@gmail.com>
2023-11-17 10:13:33 +0900

    commitDenps

--- ScrappingService/Denps.py
+++ ScrappingService/Denps.py
@@ -23,6 +23,10 @@ time.sleep(2)
 action.send_keys(Keys.END)
 time.sleep(2)
 
+#리뷰버튼부분이 없음
+
+
+# 페이지 버튼 수집
 page_div = driver.find_element(By.CLASS_NAME, 'pagination')
 page_tag = page_div.find_elements(By.TAG_NAME, 'a')
 
@@ -35,13 +39,14 @@ if page_num >= 5:
 
         # 페이지 내용 스크래핑
         bs = BeautifulSoup(driver.page_source, 'html.parser')
-        product_review = bs.find(class_="products_reviews_list")
-        product_reviews = product_review.find_all("div", "review_list_v2__message_container")
+        product_review = bs.find(class_="reviews_cont")
+        product_reviews = product_review.select('#review_61928 > div.review_list_v2__review_lcontent > div > div.review_list_v2__content_section > div > div.review_list_v2__content.review_content__collapsed > div > div > div.review_list_v2__message.js-collapsed-review-content.js-translate-text')
 
-        print(i)
+        print(product_reviews)
 
         #페이지 버튼 클릭
-        page_button = driver.find_element(By.XPATH,f'//*[@id="content"]/div/div/div[7]/div/div/a[i]')        
+        wait = WebDriverWait(driver, 10)
+        page_button = wait.until(EC.presence_of_element_located((By.CLASS_NAME, 'pagination__button.pagination__button--next')))      
         time.sleep(4)
         page_button.click()
         time.sleep(4)
@@ -49,14 +54,16 @@ if page_num >= 5:
         #페이지 내용 스크래핑
         for j in range(0, len(product_reviews)):
 
-            content_element = product_reviews[j].select_one('.review_list_v2__expand_link.js-renewal-review-message-link.js-renewal-link-expand.disabled > div')
+            content_element = product_reviews[j].select_one('#review_61928 > div.review_list_v2__review_lcontent > div > div.review_list_v2__content_section > div > div.review_list_v2__content.review_content__collapsed > div > div > div.review_list_v2__message.js-collapsed-review-content.js-translate-text')
             product_element = driver.find_element(By.XPATH, f'//*[@id="frmView"]/div/div[1]/div[1]/h3')
 
             try:
                 content = content_element.text
                 product_name = product_element.text
+            
             except AttributeError:
                 continue
+            
             
             print(content)
             #리뷰내용, 제품리뷰 리스트 추가

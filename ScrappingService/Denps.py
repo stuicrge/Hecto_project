39123bfd973d061f32a9e8f807192a914db9003f
jenny1004 <wosl1006@gmail.com>
2023-11-17 12:02:01 +0900

    Update Denps.py

--- ScrappingService/Denps.py
+++ ScrappingService/Denps.py
@@ -17,57 +17,24 @@ driver = webdriver.Chrome()
 driver.get("https://www.denpsmall.com/goods/goods_view.php?goodsNo=1000000423&crema-product-reviews-1-sort=20")
 driver.maximize_window()
 
-
 action = driver.find_element(By.CSS_SELECTOR, 'body')
 time.sleep(2)
-action.send_keys(Keys.END)
-time.sleep(2)
 
-#리뷰버튼부분이 없음
+element_to_hover_over = driver.find_element(By.CLASS_NAME, 'filter_sort_basic menu')
 
+actions = ActionChains(driver)
+actions.move_to_element(element_to_hover_over)
+actions.perform()
 
-# 페이지 버튼 수집
-page_div = driver.find_element(By.CLASS_NAME, 'pagination')
-page_tag = page_div.find_elements(By.TAG_NAME, 'a')
+actions.move_to_element_with_offset(element_to_hover_over, 0, 700)
+time.sleep(7)                            
 
-page_num=len(page_tag)
-
-if page_num >= 5:
-    page_num = 6
-
-    for i in range(1, page_num):
 
         # 페이지 내용 스크래핑
-        bs = BeautifulSoup(driver.page_source, 'html.parser')
-        product_review = bs.find(class_="reviews_cont")
-        product_reviews = product_review.select('#review_61928 > div.review_list_v2__review_lcontent > div > div.review_list_v2__content_section > div > div.review_list_v2__content.review_content__collapsed > div > div > div.review_list_v2__message.js-collapsed-review-content.js-translate-text')
+bs = BeautifulSoup(driver.page_source, 'html.parser')
+product_reviews = bs.find('div', {'class': 'review_list_v2__message.js-collapsed-review-content.js-translate-text'})
+print(product_reviews)
 
-        print(product_reviews)
 
-        #페이지 버튼 클릭
-        wait = WebDriverWait(driver, 10)
-        page_button = wait.until(EC.presence_of_element_located((By.CLASS_NAME, 'pagination__button.pagination__button--next')))      
-        time.sleep(4)
-        page_button.click()
-        time.sleep(4)
 
-        #페이지 내용 스크래핑
-        for j in range(0, len(product_reviews)):
-
-            content_element = product_reviews[j].select_one('#review_61928 > div.review_list_v2__review_lcontent > div > div.review_list_v2__content_section > div > div.review_list_v2__content.review_content__collapsed > div > div > div.review_list_v2__message.js-collapsed-review-content.js-translate-text')
-            product_element = driver.find_element(By.XPATH, f'//*[@id="frmView"]/div/div[1]/div[1]/h3')
-
-            try:
-                content = content_element.text
-                product_name = product_element.text
-            
-            except AttributeError:
-                continue
-            
-            
-            print(content)
-            #리뷰내용, 제품리뷰 리스트 추가
-            contentlist.append(content)
-            productlist.append(product_name)
-                            
 print("finish")

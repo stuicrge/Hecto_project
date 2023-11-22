dd02a9e3bdd3822d517d38427d3c4011269e12c2
jenny1004 <wosl1006@gmail.com>
2023-11-22 14:02:35 +0900

    commit01
    commit

--- API_Agent_Service/src/main/frontend/src/App.js
+++ API_Agent_Service/src/main/frontend/src/App.js
@@ -1,5 +1,7 @@
 import React, {useEffect, useState} from 'react';
 import axios from 'axios';
+import 'bootstrap/dist/css/bootstrap-grid.min.css';
+
 
 function App() {
   const [hello, setHello] = useState('')
